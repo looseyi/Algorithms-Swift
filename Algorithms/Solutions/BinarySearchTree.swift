a231e993dd0edde42ca502705ae1868164aed6e2
Edmond <liugc@xueqiu.com>
2020-08-08 21:29:15 +0800

    feat: add BST search solution

--- Algorithms/Solutions/BinarySearchTree.swift
+++ Algorithms/Solutions/BinarySearchTree.swift
@@ -51,3 +51,35 @@ extension Solution {
         return isValidBST(root, nil, nil)
     }
 }
+
+
+extension Solution {
+
+    /// 【BST 查找](https://leetcode-cn.com/problems/search-in-a-binary-search-tree)
+//    给定二叉搜索树（BST）的根节点和一个值。 你需要在BST中找到节点值等于给定值的节点。 返回以该节点为根的子树。 如果节点不存在，则返回 NULL。
+//    例如，给定二叉搜索树:
+//
+//        4
+//       / \
+//      2   7
+//     / \
+//    1   3
+//
+//    和值: 2 你应该返回如下子树:
+//
+//     2
+//    / \
+//    1   3
+
+    func searchBST(_ root: TreeNode?, _ val: Int) -> TreeNode? {
+        guard let root = root else { return nil }
+
+        if root.val == val {
+            return root
+        } else if root.val < val {
+            return searchBST(root.right, val)
+        } else {
+            return searchBST(root.left, val)
+        }
+    }
+}
